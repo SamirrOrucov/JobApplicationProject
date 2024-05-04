890243bf618e66a87584d660d9a81b9675dc659f
Qulamali <z.qulamali@gmail.com>
2024-05-05 00:44:49 +0400

    add company page

--- src/components/CompanyCard/CompanyCard.jsx
+++ src/components/CompanyCard/CompanyCard.jsx
@@ -3,13 +3,13 @@ import "./CompanyCard.scss";
 const CompanyCard = () => {
   return (
     <>
-    <div className="company_card">
+      <div className="company_card">
         <img src="/kapital_bank_logo.svg" alt="" />
         <div className="company_card_content" >
-            <h3 className="company_card_content_title">Kapital Bank</h3>
-            <p className="company_card_content_text">40 vakansiya</p>
+          <h3 className="company_card_content_title">Kapital Bank</h3>
+          <p className="company_card_content_text">40 vakansiya</p>
         </div>
-    </div>
+      </div>
     </>
   )
 }
