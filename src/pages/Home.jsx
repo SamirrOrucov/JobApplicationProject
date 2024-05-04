b6af9f5f02a0a42776b57cbe81693eb76cf40eab
AbbasovMalik <malik.abbasov03@gmail.com>
2024-05-04 18:19:37 +0400

    search filter and categories done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import FiltersComponents from "../components/HomePageComponents/FiltersComponents";
 
 
 function Home() {
   return (
     <div>
-Home Page 
+      <FiltersComponents/>
 
    </div>
   );
