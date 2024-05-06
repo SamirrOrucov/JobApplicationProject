0678101ea559ca9576d86287e5347cd90c8f44ac
AbbasovMalik <malik.abbasov03@gmail.com>
2024-05-06 22:18:43 +0400

    sirketler section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import FiltersComponents from "../components/HomePageComponents/FiltersComponents";
+import FiltersComponents from "../components/HomePageComponents/FitlerCategory";
+import Sirketler from "../components/HomePageComponents/Sirketler";
+
 
 
 function Home() {
   return (
     <div>
       <FiltersComponents/>
+      <Sirketler/>
 
    </div>
   );
