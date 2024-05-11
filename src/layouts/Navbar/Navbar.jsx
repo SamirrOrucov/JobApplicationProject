3a536aff43720096dc3b68aef0593610ce73f871
elcan900 <elcanseyfullayev05@gmail.com>
2024-05-11 15:59:52 +0400

    fixed some problems

--- src/layouts/Navbar/Navbar.jsx
+++ src/layouts/Navbar/Navbar.jsx
@@ -14,12 +14,11 @@ function Navbar() {
           <div className="menu">
             <h3>İş Elanları</h3>
             <h3>Şirkətlər</h3>
-            <h3>Kategoriyalar</h3>
             <h3>Müştəri Xidmətləri</h3>
           </div>
           <div className="login">
-            <h4>Sign In</h4>
-            <h4 className="loginbtn">Log In</h4>
+            <h4>Aliyev Ali</h4>
+            <img src="https://static-00.iconduck.com/assets.00/profile-circle-icon-2048x2048-cqe5466q.png" alt="" />
           </div>
         </nav>
       </div>
