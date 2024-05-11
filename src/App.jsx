69f9a6367ad866d6d281815fc365dc684256a0bb
jfrlysona <jfrlysona@gmail.com>
2024-05-11 16:59:43 +0400

    profile changed to userInformation

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AdminLayout from "./layouts/AdminLayout/AdminLayout";
 import MainLayout from "./layouts/MainLayout/MainLayout";
+import CompanyPage from "./pages/Admin/CompanyPage";
+import UsersPage from "./pages/Admin/UsersPage";
+import UserDetail from "./pages/Admin/UsersPage/UserDetail";
+import VacancyPage from "./pages/Admin/VacancyPage";
+import Companies from "./pages/Companies/Companies";
+import CompaniesDetail from "./pages/Companies/CompaniesDetail/CompaniesDetail";
 import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import NoPage from "./pages/NoPage/NoPage";
-import Companies from "./pages/Companies/Companies";
-import CompaniesDetail from "./pages/Companies/CompaniesDetail/CompaniesDetail";
 import SignInPage from "./pages/SignInPage/SignInPage";
-import AdminLayout from "./layouts/AdminLayout/AdminLayout";
-import VacancyPage from "./pages/Admin/VacancyPage";
-import UsersPage from "./pages/Admin/UsersPage";
-import UserDetail from "./pages/Admin/UsersPage/UserDetail";
-import CompanyPage from "./pages/Admin/CompanyPage";
-import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import UserInformationPage from "./pages/UserInformationPage/UserInformationPage";
 
 function App() {
   return (
@@ -23,7 +23,7 @@ function App() {
             <Route path="/companies" element={<Companies />} />
             <Route path="/companies/detail" element={<CompaniesDetail />} />
             <Route path="/*" element={<NoPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/profile" element={<UserInformationPage />} />
             <Route path="/sign-in" element={<SignInPage />} />
             <Route path="/login" element={<LoginPage />} />
           </Route>
--- src/components/Profile/Profile.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./Profile.scss";
-function Profile() {
-  return <div className="profile-container">Profile</div>;
-}
-
-export default Profile;
